66261722aa049d111455004b08ec36e5b14409b0
eduarda virgilio <eduarda.virgilio2630@gmail.com>
2025-08-23 16:36:04 -0300

    todos testes feitos (?)

--- test_juros_compostos.py
+++ test_juros_compostos.py
@@ -5,7 +5,7 @@ from juros_composto import calcular_juros_compostos
 @pytest.mark.parametrize("capital, juros, tempo, situacao_esperada",
                          [
                              (1000, 20, 1, (200, 1200)),
-                             (1000, 30, 1, (300, 1300))
+                             (1000, 15.5, 1, (155, 1155))
                          ])
 
 
@@ -52,9 +52,9 @@ def test_tempo_negativo():
 # vendo se o capital é uma str
 def test_string_capital():
     # Definindo a entrada
-    capital = "alex te amo"
-    juros = 10
-    tempo = 5
+    capital = "ola"
+    juros = 40
+    tempo = 2
 
     # Executando a função e esperando erro
     with pytest.raises(TypeError, match="O capital investido deve ser um número (int ou float)."):
@@ -64,20 +64,63 @@ def test_string_capital():
 def test_string_juros():
     # Definindo a entrada
     capital = 1000
-    juros = "banana"
-    tempo = 5
+    juros = "ola"
+    tempo = 2
 
     # Executando a função e esperando erro
     with pytest.raises(TypeError, match="A taxa de juros deve ser um número (int ou float)."):
         calcular_juros_compostos(capital, juros, tempo) 
 
 # vendo se o tempo é uma str
-def test_string_juros():
+def test_string_tempo():
     # Definindo a entrada
     capital = 1000
-    juros = 10
+    juros = 40
     tempo = "ola"
 
     # Executando a função e esperando erro
     with pytest.raises(TypeError, match="O tempo deve ser um número (int ou float)."):
         calcular_juros_compostos(capital, juros, tempo) 
+
+# vendo se o capital é 0
+def test_zero_capital():
+    # Definindo a entrada
+    capital = 0
+    juros = 40
+    tempo = 2
+
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="O capital deve ser um número maior que 0"):
+        calcular_juros_compostos(capital, juros, tempo) 
+
+# vendo se o juros é 0
+def test_zero_juros():
+    # Definindo a entrada
+    capital = 1000
+    juros = 0
+    tempo = 2
+
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="O juros deve ser um número maior que 0"):
+        calcular_juros_compostos(capital, juros, tempo) 
+
+# vendo se o tempo é 0
+def test_zero_tempo():
+    # Definindo a entrada
+    capital = 1000
+    juros = 40
+    tempo = 0
+
+    # Executando a função e esperando erro
+    with pytest.raises(ValueError, match="O tempo deve ser um número maior que 0"):
+        calcular_juros_compostos(capital, juros, tempo) 
+
+# vendo se foi enviado menos de 3 valores
+def test_menos_de_tres_valores():
+    # Definindo a entrada
+    capital = 1000
+    juros = 40
+
+    # Executando a função e esperando erro
+    with pytest.raises(TypeError, match="Não é permitido enviar menos que 3 valores"):
+        calcular_juros_compostos(capital, juros)
